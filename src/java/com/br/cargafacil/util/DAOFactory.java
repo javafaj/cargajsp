cf0667d880d3b1a9245488c6ef3c0ce0b5ef587a
pepag <pepag@DESKTOP-12J69K0>
2015-10-22 22:11:29 -0200

    Cadastre-se gravando os dados no banco

--- src/java/com/br/cargafacil/util/DAOFactory.java
+++ src/java/com/br/cargafacil/util/DAOFactory.java
@@ -1,13 +1,15 @@
+
+
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- *//*
+ */
 package com.br.cargafacil.util;
 
-import br.com.senacrs.dao.JDBCPessoaDao;
-import br.com.senacrs.dao.PessoaDao;
-
+import com.br.cargafacil.dao.UsuarioDAO;
+import com.br.cargafacil.dao.UsuarioJDBC;
 /**
  *
  * @author pepag
@@ -15,12 +17,11 @@ import br.com.senacrs.dao.PessoaDao;
  * Esta classe somente retorna objetos das classes JDBC  para as classes DAO
    caso seja necessario realizar a troca de tecnologia JDBC para hibernate , basta realizar a troca dos objetos desta classe e pronto não é necessario 
    * realizar a troca nas classes DAOS
- *//*
+ */
 public class DAOFactory {
-    public static PessoaDao createPessoaDAO(){
+    public static UsuarioDAO createUsuarioDAO(){
     
-                return new JDBCPessoaDao();
+                return new UsuarioJDBC();
     }
     
 }
-*/
